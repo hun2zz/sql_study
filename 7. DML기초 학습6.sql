7dcd4fa6c5751b969f65cce5538c4f20f7cd9472
hun2zz <tkdgnsdldkdlel@naver.com>
2024-05-01 12:28:17 +0900

    그룹 바이

--- 7. DML기초 학습6.sql
+++ 7. DML기초 학습6.sql
@@ -81,3 +81,132 @@ SELECT -- 3번 부서이면서 남자인 얘를 찾는다. GROUP이 2개라면.
     ORDER BY dept_cd
     ;
     
+    --사원별로 2019년에 급여 평균액, 최소 지급액, 최대지급액 조회
+    -- WHERE : 집계 전 필터링
+    SELECT 
+    emp_no, 
+    TO_CHAR(SUM(pay_amt), 'L999,999,999') "사원별 총급여액",
+    TO_CHAR(ROUND(AVG(pay_amt), 2), 'L999,999,999.99') "사원별 평균급여액",
+    TO_CHAR(MAX(pay_amt), 'L999,999,999') "사원별 최고급여액",
+    COUNT(pay_de) "급여 수령횟수"
+FROM tb_sal_his
+WHERE pay_de BETWEEN '20190101' AND '20191231'
+GROUP BY emp_no
+ORDER BY emp_no
+;
+
+--HAVING : 집계 후 필터링
+SELECT 
+    emp_no, 
+    TO_CHAR(SUM(pay_amt), 'L999,999,999') "사원별 총급여액",
+    TO_CHAR(ROUND(AVG(pay_amt), 2), 'L999,999,999.99') "사원별 평균급여액",
+    TO_CHAR(MAX(pay_amt), 'L999,999,999') "사원별 최고급여액",
+    COUNT(pay_de) "급여 수령횟수"
+FROM tb_sal_his
+WHERE pay_de BETWEEN '20190101' AND '20191231'
+GROUP BY emp_no
+HAVING AVG(pay_amt) >= 4000000
+ORDER BY emp_no
+;
+
+--? 부서별로 가장 어린사람의 생년월일, 연장자의 생년월일, 부서별 총 사원 수를 조회
+--? 그런데 부서별 사원이 1명인 부서의 정보는 조회하고 싶지 않음.
+
+SELECT
+    dept_cd,
+    MAX(birth_de),
+    MIN(birth_de),
+    COUNT(emp_no)
+FROM tb_emp
+GROUP BY dept_cd
+HAVING COUNT(*) > 1
+ORDER BY dept_cd
+;
+
+-- ORDER BY : 정렬
+-- ASC : 오름차 정렬 (기본값), DESC : 내림차 정렬
+-- 항상 SELECT절의 맨 마지막에 위치
+
+SELECT 
+    emp_no
+    , emp_nm
+    , addr
+FROM tb_emp
+ORDER BY emp_no
+;
+
+SELECT 
+    emp_no
+    , emp_nm
+    , addr
+FROM tb_emp
+ORDER BY emp_nm ASC
+;
+
+SELECT 
+    emp_no
+    , emp_nm
+    , dept_cd
+FROM tb_emp
+ORDER BY dept_cd, emp_nm DESC
+;
+
+
+SELECT 
+    emp_no AS 사번
+    , emp_nm AS 이름
+    , addr AS 주소
+FROM tb_emp
+ORDER BY 이름 DESC
+;
+
+SELECT 
+    emp_no
+    , emp_nm
+    , dept_cd
+FROM tb_emp
+ORDER BY 3 ASC, 1 DESC
+;
+
+
+SELECT 
+    emp_no
+    , emp_nm
+    , dept_cd
+FROM tb_emp
+ORDER BY 3 ASC, emp_no DESC
+;
+
+SELECT emp_no AS 사번, emp_nm AS 이름, addr AS 주소
+FROM tb_emp
+ORDER BY 이름, 1 DESC
+;
+
+SELECT 
+    emp_no
+    , SUM(pay_amt) 연봉
+FROM tb_sal_his
+WHERE pay_de BETWEEN '20190101' AND '20191231'
+GROUP BY emp_no
+HAVING AVG(pay_amt) >= 4500000
+ORDER BY emp_no
+;
+
+SELECT 
+	EMP_NM ,
+	DIRECT_MANAGER_EMP_NO 
+FROM TB_EMP
+ORDER BY DIRECT_MANAGER_EMP_NO DESC
+;
+
+
+-- 사원별로 2019년 월평균 수령액이 450만원 이상인 사원의 사원번호와 2019년 연봉 조회
+SELECT 
+    emp_no
+    , SUM(pay_amt) 연봉
+FROM tb_sal_his
+WHERE pay_de BETWEEN '20190101' AND '20191231'
+GROUP BY emp_no
+HAVING AVG(pay_amt) >= 4500000
+ORDER BY SUM(pay_amt) DESC
+;
